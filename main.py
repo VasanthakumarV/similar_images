7b5b612fe53b1c10b59ee1c119827ab4c7284fcf
VasanthakumarV <vasanth260m12@gmail.com>
2021-01-20 14:11:49 +0530

    Add missing code comments

--- main.py
+++ main.py
@@ -31,7 +31,9 @@ BATCH_SIZE = 32
 TRAIN_SIZE = 0.9
 LEARNING_RATE = 1e-3
 
+# Number of channels in the input image
 CHANNELS_IN = 3
+# Number of the channels in the encoder output
 CHANNELS_OUT = 256
 
 EMBEDDING_CHKPT = "embedding_epoch_%d.npy"
@@ -135,12 +137,20 @@ def create_embedding(model, data_loader, device):
 
 
 def main(dataset, device):
+    """Helper function to train the model and save the training
+    examples' embedding matrix
+    """
+    # Initializing the model
     model = SimilarityModel(cin=CHANNELS_IN, cout=CHANNELS_OUT).to(device)
+
+    # Initializing the optimizer
     optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
 
+    # Calculating the training and validation data sizes
     train_size = int(len(dataset) * TRAIN_SIZE)
     val_size = len(dataset) - train_size
 
+    # Creating torch's Dataset classes for training and validation
     train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
     # NOTE `Shuffle` is set to `False`, the ordering must be preserved
@@ -159,31 +169,41 @@ def main(dataset, device):
             val_loss = validate(model, val_loader, device)
             print(f"Epoch: {epoch}, Validation Loss: {val_loss:.4f}")
 
-            print("Saving model and embedding")
-
+            # Encoding all the training images and flattening them
             embedding = create_embedding(model, full_loader, device)
             embedding = embedding.cpu().detach().numpy()
 
+            print("Saving model and embedding")
             np.save(EMBEDDING_CHKPT % epoch, embedding)
             torch.save(model.state_dict(), MODEL_CHKPT % epoch)
 
 
 def test(dataset, test_data, device):
+    """Helper function to find similar images for new images"""
     imgs = glob.glob(os.path.join(test_data, "*.jpg"))
     nimgs = len(imgs)
 
+    # Reading in the testing data
     data_npy = np.array([np.array(Image.open(img)) for img in imgs])
+
+    # Converting the read data in torch tensors
+    # and making sure the second dimension represents the channel size
     data = torch.from_numpy(data_npy).permute(0, 3, 1, 2).float()
 
+    # Loading in the training examples' embedding matrix
     embedding = np.load(EMBEDDING_FINAL)
+
+    # Loading in the trained model
     model = SimilarityModel(cin=CHANNELS_IN, cout=CHANNELS_OUT).to(device)
     model.load_state_dict(torch.load(MODEL_FINAL, map_location=device))
     model.eval()
 
     with torch.no_grad():
+        # Encoding the testing images
         encoding = model.encoder(data.to(device)).reshape(
             nimgs, -1).cpu().detach().numpy()
 
+    # Here we find the similar images for each testing image and plot them
     for i in range(nimgs):
         indices = nearest_neighbors(
             9,
